211a4a25dc58027e3fdaac14b34bb62d5add7106
ayanfe-arch <ayanfe123ayanfe@gmail.com>
2026-04-17 23:17:14 +0100

    debug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
-.catch((err) => console.log('Connection failed', err))
+.catch((err) => console.log('Connection failed', err.message))
 app.use(express.json())
 app.use(cors());
 app.use('/api/profiles', require('./routes/profile'));
